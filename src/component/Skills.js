c59a3423ef89cc11a114bf815c6944578754c5eb
Madhu Yadav <29pawanyadav@gmail.com>
2024-09-05 19:09:29 +0530

    Proftlio my

---
+++ src/component/Skills.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Skills = () => (
+  <section id="skills">
+    <h2>Skills</h2>
+    <ul>
+      <li>React.js</li>
+      <li>JavaScript</li>
+      <li>HTML & CSS</li>
+      <li>Bootstrap</li>
+      <li>Angular</li>
+      <li>Node.js</li>
+      <li>MongoDB</li>
+      <li>Postman</li>
+      <li>Express.js</li>
+      <li>Next.js</li>
+      <li>C/C++</li>
+      <li>Python</li>
+      <li>Data Visualization & Analysis</li>
+      <li>Data Structures and Algorithms</li>
+    </ul>
+  </section>
+);
+
+export default Skills;
